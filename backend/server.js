61769f88d4520cb4a9a7c5439bb387193c57a0c9
zia221 <welcome2zia@gmail.com>
2026-01-21 15:38:59 +0500

    adding vercel json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express?.json());
 
 // Connect to Database
-await connectDB(); // Call the function
+connectDB(); // Call the function
 
 // Routes
 app.use("/api/counts", require("./routes/countRoutes"));
